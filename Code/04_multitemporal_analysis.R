1717a9b20536b7fe48be6f47ac86f1f2628e3914
ericamerici <ericamerici@gmail.com>
2026-03-19 09:15:31 -0500

    Create 04_multitemporal_analysis.R

---
+++ Code/04_multitemporal_analysis.R
@@ -0,0 +1,23 @@
+# Code for performing multitemporal analysis with satellite imagery
+
+library(terra)
+library(imageRy)
+
+im.list()
+
+EN_01 <- im.import("EN_01.png")
+EN_01 <- flip(EN_01)
+plot(EN_01)
+
+EN_13 <- im.import("EN_13.png")
+EN_13 <- flip(EN_13)
+plot(EN_13)
+
+# Plot the two images one on top of the other one
+im.multiframe(2,1)
+plot(EN_01)
+plot(EN_13)
+
+# Differencing
+ENdif <- EN_01[[1]]-EN_13[[1]]
+plot(ENdif)
